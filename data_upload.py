e6d855a2933e2b72365f6fde5d1ac6b66668a9d5
Ankith Kumar S H <2003ankithkumar@gmail.com>
2025-04-22 20:37:40 +0530

    Initial commit

--- data_upload.py
+++ data_upload.py
@@ -3,12 +3,4 @@ import streamlit as st
 import numpy as np
 
 
-#model = pickle.load(open('models/model.pkl', 'rb'))
-# collab_data = pickle.load(open('models/collab_data.pkl', 'rb'))
-#collab_pivot = pickle.load(open('models/collab_pivot.pkl', 'rb'))
-#  req_content_data = pickle.load(open('models/req_content_data.pkl', 'rb'))
-#books_names_list = pickle.load(open('models/books_names_list.pkl', 'rb'))
-#req_updated_books = pickle.load(open('models/req_updated_books.pkl', 'rb'))
-#content_similarity = pickle.load(open('models/content_similarity.pkl', 'rb'))
-
 print("Begin")
